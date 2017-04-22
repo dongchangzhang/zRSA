fd53290e0b05a1b279b3ec2bff53094496a8dd84
张东昌 <dongchangzhang@gmail.com>
2017-04-22 12:29:43 +0800

    init the project

---
+++ cmake-build-debug/CMakeFiles/zRSA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zRSA.dir/zrsa.cpp.obj"
+  "zRSA.pdb"
+  "zRSA.exe"
+  "zRSA.exe.manifest"
+  "libzRSA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zRSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
